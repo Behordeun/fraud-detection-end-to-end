ad402073c190df3a7f8e1b472a4f8f4cf1f58672
Muhammad Abiodun SULAIMAN <abiodun.msulaiman@gmail.com>
2025-01-09 00:38:32 +0100

    🐛 chore(conftest): refactor Spark session creation
    Refactored the Spark session creation in the conftest to be more readable and consistent. In the previous implementation, there was an error as the chaining of methods was not correctly formatted. This change ensures that the creation of the Spark session is done in a clearer and more uniform manner, promoting code maintainability.

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,9 +7,10 @@ def spark():
     """
     Fixture for creating a shared Spark session for tests.
     """
-    spark = SparkSession.builder \
-        .appName("TestSparkSession") \
-        .master("local[1]") \
+    spark = (
+        SparkSession.builder.appName("TestSparkSession")
+        .master("local[1]")
         .getOrCreate()
+    )
     yield spark
     spark.stop()
