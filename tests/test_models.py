6e631fdd9d3070ec8ee5c2ea4ced591e67f6ee99
Muhammad Abiodun SULAIMAN <abiodun.msulaiman@gmail.com>
2025-01-09 09:08:30 +0100

    chore(tests): update sys.path in test_models
    Added project root directory to sys.path for better module import handling. Resolves potential import issues in the tests.

--- tests/test_models.py
+++ tests/test_models.py
@@ -5,11 +5,12 @@ import pytest
 from pyspark.ml.classification import RandomForestClassifier
 from pyspark.sql import SparkSession
 
-# Add the src directory to the Python path
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../src")))
-
 from src.models.evaluate import evaluate_model
 from src.models.train import train_model
+
+
+# Add the project root directory to sys.path
+sys.path.append(str(Path(__file__).resolve().parent.parent))
 
 
 @pytest.fixture(scope="module")
