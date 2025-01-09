924cf2167481c74fac8df6be822c967ff85cb5b0
Muhammad Abiodun SULAIMAN <abiodun.msulaiman@gmail.com>
2025-01-09 08:08:07 +0100

    build(tests): add src directory to sys.path
    Inserted the src directory path into sys.path in tests to enable importing of modules from the src directory.

--- tests/test_data_preprocessing.py
+++ tests/test_data_preprocessing.py
@@ -1,5 +1,9 @@
 import pytest
 from pyspark.sql import SparkSession
+import sys
+import os
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../src')))
 from src.data_processing.preprocess import preprocess_data
 from src.data_processing.feature_engineering import add_derived_features
 
