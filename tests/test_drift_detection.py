3813f9439e81b33f4030dbe920e539f3c42d0060
Muhammad Abiodun SULAIMAN <abiodun.msulaiman@gmail.com>
2025-01-09 08:35:45 +0100

    ➕ chore(tests): add Python path configuration in test_drift_detection
    Add the src directory to the Python path for better module accessibility in tests/test_drift_detection.py. This change ensures that the related modules can be seamlessly imported and utilized within the testing environment.

--- tests/test_drift_detection.py
+++ tests/test_drift_detection.py
@@ -6,7 +6,9 @@ from pyspark.ml.classification import LogisticRegression
 from pyspark.ml.linalg import Vectors
 from pyspark.sql import Row, SparkSession
 
+# Add the src directory to the Python path
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../src")))
+
 from src.monitoring.data_drift import calculate_statistics, compare_distributions
 from src.monitoring.model_drift import evaluate_model
 
