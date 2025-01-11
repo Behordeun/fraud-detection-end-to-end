032614a5755c5deb0cc84064d174d42a31ae43ee
Muhammad Abiodun SULAIMAN <abiodun.msulaiman@gmail.com>
2025-01-11 08:49:20 +0100

    ✅ feat(tests): dynamically add 'src' to sys.path
    Dynamically add `src` to sys.path for test imports. This change allows easier importing of modules from the `src` directory into tests by adding the `src` directory to Python's search path.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,12 @@
+import sys
+from pathlib import Path
+
 import pytest
+
+# Dynamically add `src` to sys.path for test imports
+project_root = Path(__file__).resolve().parent.parent  # Points to the project root
+sys.path.insert(0, str(project_root / "src"))  # Add `src` to Python's search path
+
 from pyspark.sql import SparkSession
 
 
